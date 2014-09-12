47265b2b4360ddacf4c864be618616d79f35895e
StephR <stephane.rapin@neuf.fr>
2014-09-13 01:36:07 +0200

    Ajout de commentaires sur le controleur par défaut (test phpdox)

--- src/Somfy/EasyReportBundle/Controller/DefaultController.php
+++ src/Somfy/EasyReportBundle/Controller/DefaultController.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+ * This file is part of the 3asyReport package.
+ *
+ * (c) Stephane Rapin <stephane.rapin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Somfy\EasyReportBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Index page of 3asyReport.
+ *
+ * @author Stephane Rapin <stephane.rapin@gmail.com>
+ */
 class DefaultController extends Controller
 {
     public function indexAction()
